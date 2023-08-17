18d89e7067b0339f02227bc1b30cd85e4a9be34d
student <student@gnu.ac.in>
2023-08-17 12:34:49 +0530

    Practical 4 is combine 1

---
+++ app/src/main/java/com/rutvikpatel/practical_3_b6/Registratiopage.kt
@@ -0,0 +1,11 @@
+package com.rutvikpatel.practical_3_b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registratiopage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registratiopage)
+    }
+}
---
+++ app/src/main/java/com/rutvikpatel/practical_3_b6/homepage.kt
@@ -0,0 +1,15 @@
+package com.rutvikpatel.practical_3_b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class homepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+
+    fun signup(view: View) {}
+    fun login(view: View) {}
+}
